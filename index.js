99a579cdfcff54eeded18fc5d5796369465d97d2
ravi504 <ravinder.rsr@gmail.com>
2025-06-27 22:39:32 +0530

    Add the year

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send({hi : 'Current node express for June 25' });
+    res.send({hi : 'Current node express for June 2025' });
 });
 
 const PORT = process.env.PORT || 5000;
